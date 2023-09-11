eca65aa5ce8dcef3e81c8ea2f78f0b150fb4b5ec
FoulTrip <davidvasquezmahecha@gmail.com>
2023-09-11 13:34:50 -0700

    first commit

---
+++ binary_tree_print.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * binary_tree_print - Prints a binary tree
+ * @tree: Pointer to the root node of the tree to print
+ */
+void binary_tree_print(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_print(tree->right);
+	printf("%d\n", tree->n);
+	binary_tree_print(tree->left);
+}
