0064a778c6500dcfbb255e054b75af028c89b71d
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-09-13 21:13:28 -0500

    tree_height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,18 +8,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0
+	size_t left_height, right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	while (tree->left || tree->right)
-	{
-		height++;
-		if (binary_tree_balance(tree) > 0)
-			tree = tree->left;
-		else
-			tree = tree->right;
-	}
-	return (height);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
