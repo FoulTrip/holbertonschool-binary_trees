4b07081b499563ccfb3480413464d5dedf54bd36
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2023-09-14 00:53:50 -0500

    tree_nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,7 +6,7 @@
  *
  * Return: Number of nodes with at least 1 child in the tree, or 0 if tree is NULL;
  */
-size_t binary_tree_nodes(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nodes = 0;
 
